9eb5e21c6d2965420cfbc8e5b77429f34f182844
nanamikano <77386004+nanamikano@users.noreply.github.com>
2021-01-27 15:59:47 +0900

    Add files via upload

---
+++ Homework 1.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+favoritecolors=['blue','purple','black','white','green','maroon']
+print(favoritecolors)
+
+
+# In[5]:
+
+
+nums=list(range(61))
+print(nums[30:61:3])
+
+
+# In[6]:
+
+
+dictionary={'Sunny':'play','Rainy':'Watching TV', 'Cloudy':'walk'}
+print(dictionary)
+
+
+# In[7]:
+
+
+dictionary.update({'Snowy':'ski'})
+print(dictionary)
+
+
+# In[12]:
+
+
+grade=82
+
+if grade >=90:
+    print('A')
+elif grade>= 80:
+    print('B')
+elif grade >= 70:
+    print('C')
+else:
+    print('F')
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
